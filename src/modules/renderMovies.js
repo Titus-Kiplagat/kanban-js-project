521e2b07e1315764645e8d891109a00646986b01
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-09-09 09:56:57 +0000

    Fixed linters errors

--- src/modules/renderMovies.js
+++ src/modules/renderMovies.js
@@ -1,5 +1,6 @@
 import allItemsCount from './allItemsCount.js';
 import cardMarkup from './cardMarkup.js';
+import displayPopup from './displayPopup.js';
 import fetchAndDisplayLikes from './fetchAndDisplayLikes.js';
 import fetchData from './fetchData.js';
 import handleLikeBtnClick from './handleLikeBtnClick.js';
@@ -18,6 +19,18 @@ const renderMovies = async () => {
   }).catch((error) => {
     throw new Error(error.message);
   });
+  const openPopup = () => {
+    const commentBtns = document.querySelectorAll('.btn-comments');
+    commentBtns.forEach((btn) => {
+      btn.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const commentId = btn.getAttribute('id');
+        displayPopup(commentId);
+      });
+    });
+  };
+
+  openPopup();
 
   handleLikeBtnClick();
   allItemsCount();
