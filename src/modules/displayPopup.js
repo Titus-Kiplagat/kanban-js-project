de9fb26a7cd4f791da843eaa1ec740f3ad8cca72
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-09-09 09:53:17 +0000

    Add functionality display popup for the modal

---
+++ src/modules/displayPopup.js
@@ -0,0 +1,24 @@
+import createPopup from './createPopupMarkup.js';
+
+import { detailsAll, popupCloseBtn } from './handlePopupCloseBtn.js';
+
+const displayPopup = async (id) => {
+  const allpopup = document.querySelector('.popupContainer');
+  const { body } = document;
+  body.style.overflow = 'hidden';
+
+  allpopup.innerHTML = '';
+
+  const movieDetail = await detailsAll(id);
+  allpopup.append(createPopup(movieDetail));
+  allpopup.style.display = 'flex';
+
+  //   const commentLine = document.querySelector('.commentDisplay');
+  //   const comments = await createComment(id);
+  //   commentLine.append(comments);
+
+  popupCloseBtn();
+  // createNewComment();
+};
+
+export default displayPopup;
